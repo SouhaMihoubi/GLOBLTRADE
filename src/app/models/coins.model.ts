675ed6aa573027b2b7ed39aeb7017c1e28c448f4
unknown <medhadiji.mh@gmail.com>
2019-01-23 13:59:57 +0100

    commit test

---
+++ src/app/models/coins.model.ts
@@ -0,0 +1,11 @@
+export interface Coins {
+  
+    position: number;
+    coin: string;
+    price: number;
+    directvol: number;
+    totalvol: number;
+    marketcap: number;
+    chg_24:number;
+    
+  }
