0fbb08566ab2b00082e271f5576c54e8ba584827
souhaMihoubi <souha01102015@gmail.com>
2019-01-24 11:05:33 +0100

    table filter

--- src/app/crypto-api.service.ts
+++ src/app/crypto-api.service.ts
@@ -6,12 +6,12 @@ import { Crypto } from '../app/models/cryptoModele';
   providedIn: 'root'
 })
 export class CryptoAPIService {
-  private cryptoUrl = 'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD';
+  private cryptoUrl = 'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=100&tsym=USD' //'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR';
 
   constructor(private Http: HttpClient) { }
 
-  getCrypto(): Observable<Crypto[]> {
-    return this.Http.get<Crypto[]>(this.cryptoUrl);
+  getCrypto(): Observable<Crypto> {
+    return this.Http.get<Crypto>(this.cryptoUrl);
     //return this.http.get('https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD');
     //return this.http.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH&tsyms='+coin+'USD,EUR&api_key='+'55447dcf063b6c88eed9387897a338c5699a77ea9a8c2c6d7fe00401046de288' );
     // return this.http.get('https://min-api.cryptocompare.com/data/pricehistorical?fsym='+coin+'&tsyms=USD&ts=1452680400&api_key='+'55447dcf063b6c88eed9387897a338c5699a77ea9a8c2c6d7fe00401046de288' );
